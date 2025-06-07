e3b0bd1d5e19a5ac663a2c66bf5e9d32ec9866fa
SeokjinDev <tjrwls1108011@gmail.com>
2025-06-07 17:24:22 +0900

    [SETTING] Spring Boot

---
+++ milkyway/src/main/java/com/project/milkyway/MilkywayApplication.java
@@ -0,0 +1,13 @@
+package com.project.milkyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkywayApplication.class, args);
+	}
+
+}
