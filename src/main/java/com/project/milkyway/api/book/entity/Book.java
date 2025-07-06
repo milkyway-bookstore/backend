133cab96ef0de5cd7c085ea80d1f793f3af45d81
SeokjinDev <tjrwls1108011@gmail.com>
2025-07-06 16:41:20 +0900

    [FIX] Book Entity

---
+++ src/main/java/com/project/milkyway/api/book/entity/Book.java
@@ -0,0 +1,45 @@
+package com.project.milkyway.api.book.entity;
+
+import com.project.milkyway.api.member.entity.Member;
+import com.project.milkyway.api.member.entity.Status;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="book")
+@Builder(toBuilder=true)
+@AllArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    // 임시로 member id 입력
+    private Long pubId;
+
+    private String title;
+
+    private String desc;
+    private String author;
+
+    private Integer price;
+
+    private UUID coverImg;
+    private Integer status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Book changeStatus(BookStatus status) {
+        return this.toBuilder()
+                .status(status.getValue())
+                .build();
+    }
+}
